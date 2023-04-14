5140b52522a1fae0ba4b3a81b549360963982648
Azat Muzapbar <azamuzapbar@gmail.com>
2023-04-14 18:21:56 +0600

    решение первой задачи

--- hello/hello/urls.py
+++ hello/hello/urls.py
@@ -15,8 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
+from hello.views import add, subtract, multiply, divide
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('add/', add, name='add'),
+    path('subtract/', subtract, name='subtract'),
+    path('multiply/', multiply, name='multiply'),
+    path('divide/', divide, name='divide'),
+    path('', include('webapp.urls')),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
